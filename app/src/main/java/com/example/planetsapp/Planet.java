caf12caa8bac998158081bedb6cf838bd7840e76
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-05-24 15:54:29 -0400

    Planet Model

--- app/src/main/java/com/example/planetsapp/Planet.java
+++ app/src/main/java/com/example/planetsapp/Planet.java
@@ -1,4 +1,42 @@
 package com.example.planetsapp;
 
 public class Planet {
+
+//    1. Attributes
+    private String planetName;
+    private String moonCount;
+    private  int planetImage;
+
+//    2. Constructors
+    public Planet(String planetName, String moonCount, int planetImage) {
+        this.planetName = planetName;
+        this.moonCount = moonCount;
+        this.planetImage = planetImage;
+    }
+
+//    3. Getters & Setters
+
+    public String getPlanetName() {
+        return planetName;
+    }
+
+    public void setPlanetName(String planetName) {
+        this.planetName = planetName;
+    }
+
+    public String getMoonCount() {
+        return moonCount;
+    }
+
+    public void setMoonCount(String moonCount) {
+        this.moonCount = moonCount;
+    }
+
+    public int getPlanetImage() {
+        return planetImage;
+    }
+
+    public void setPlanetImage(int planetImage) {
+        this.planetImage = planetImage;
+    }
 }
---
+++ app/src/main/java/com/example/planetsapp/PlanetAdapter.java
@@ -0,0 +1,4 @@
+package com.example.planetsapp;
+
+public class PlanetAdapter {
+}
